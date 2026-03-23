cff2ee615662a7e9584851b02724ad6802e754e1
VidadiAli <vidadiali.tiva@gmail.com>
2026-03-23 22:12:14 +0400

    montly amount

--- src/Components/Main/PageLayout/ProductCard.jsx
+++ src/Components/Main/PageLayout/ProductCard.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { addLikeds, addToBasket, callLocalBasket, unLiked } from "../../../functions";
-import { FaStar, FaHeart, FaRegHeart } from "react-icons/fa";
-import AuthForm from "../../Register/AuthForm";
+import { FaStar, FaHeart, FaRegHeart, FaCalendarAlt } from "react-icons/fa";
+import { percentage } from "../../Data/DataFile";
 
 const ProductCard = ({
     item,
@@ -11,7 +11,8 @@ const ProductCard = ({
     profileInfo }) => {
 
     const [loading, setLoading] = useState(false);
-    const [customerToken, setCustomerToken] = useState(null)
+    const [month, setMonth] = useState(3);
+    const [percentageValue, setPercentageValue] = useState(4.2)
 
     const finalPrice = item.hasDiscount
         ? (item.price - (item.price * item.discountPercent) / 100).toFixed(2)
@@ -45,9 +46,32 @@ const ProductCard = ({
                     <p className="vns-product-company">{item.salesCompany}</p>
                     <h3 className="vns-product-name">{item.itemName}</h3>
 
-                    <p className="item-rating">
-                        {((item?.price + (item?.price * 21.6) / 100) / 18).toFixed(2)} ₼ x 18 ay
-                    </p>
+                    <div className="vns-product-installment">
+                        <h3>
+                            <FaCalendarAlt /> Aylıq ödəniş
+                        </h3>
+
+                        <div className="vns-product-month-options">
+                            {percentage.map(m => {
+                                if ([3, 6, 18, 35].includes(m.percentageMonth)) {
+                                    return <button
+                                        key={m?.id}
+                                        className={month == m?.percentageMonth ? "active" : ""}
+                                        onClick={() => {
+                                            setMonth(m?.percentageMonth);
+                                            setPercentageValue(m?.percentage)
+                                        }}
+                                    >
+                                        {m?.percentageMonth}
+                                    </button>
+                                }
+                            })}
+                        </div>
+
+                        <div className="vns-product-monthly-result">
+                            <strong>{month} ay x {((((item.price * percentageValue) / 100) + item.price) / month).toFixed(2)} ₼</strong>
+                        </div>
+                    </div>
                     <div className="vns-product-price-row">
                         <span className="vns-product-price">{finalPrice} ₼</span>
                         {item.hasDiscount && (
